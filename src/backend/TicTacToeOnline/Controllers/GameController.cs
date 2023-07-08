c9bae0e2ccd0e9b5c375e6015c8774122232642b
Felipe <felipesantos.dsb@hotmail.com>
2023-07-07 23:52:45 -0300

    3. API Implementation

--- src/backend/TicTacToeOnline/Controllers/GameController.cs
+++ src/backend/TicTacToeOnline/Controllers/GameController.cs
@@ -6,17 +6,52 @@ namespace TicTacToeOnline.Controllers
     [ApiController]
     public class GameController : ControllerBase
     {
+        private static List<Game> games = new List<Game>();
+
         [HttpPost("new")]
         public ActionResult<string> NewGame()
         {
-            //pass
+            var gameId = GenerateNewGameId();
+            var game = new Game
+            {
+                Id = gameId,
+                Board = new char[3, 3],
+                CurrentPlayer = 'X'
+            };
+            games.Add(game);
+
+            return Ok(gameId);
         }
 
-        [HttpPost("{gameId}/move")]
-        public ActionResult MakeMove(string gameId, [FromBody] MoveRequest moveRequest)
+        private string GenerateNewGameId()
         {
-            //pass
-            return Ok("move_result");
+            return Guid.NewGuid().ToString();
+        }
+
+
+        private Game GetGameById(string gameId)
+        {
+            return games.FirstOrDefault(game => game.Id == gameId);
+        }
+
+
+        [HttpPost("{gameId}/move")]
+        public ActionResult MakeMove(string gameId, [FromBody] Move move)
+        {
+            var game = GetGameById(gameId);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            if (!IsValidMove(game, move))
+            {
+                return BadRequest("Invalid move");
+            }
+            game.Board[move.Row, move.Column] = game.CurrentPlayer;
+
+            var gameResult = GetGameResult(game);
+            
+            return Ok(gameResult);
         }
     }
 }
